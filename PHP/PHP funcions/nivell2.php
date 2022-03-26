8ac1347d83f26b40957bf775bbca52a6b5b65778
edverde <eduardvr83@gmail.com>
2022-03-26 12:43:34 +0100

    nivell 2 acabat

--- PHP/PHP funcions/nivell2.php
+++ PHP/PHP funcions/nivell2.php
@@ -41,6 +41,39 @@
     rebut(2.3);
     rebut(1);
 
+     // <!-- ----------------------------------------------------------------------------
+    // exercici 3 
+    // ---------------------------------------------------------------------------- -->   
+    
+    echo '<h2><u><strong>Exercici 3 </strong></u></h2><br>';
+    echo "Imagina que som a una botiga on es ven:<br><br>
+
+    Xocolata: 1 euro<br>
+    Xiclets: 0.50 euros<br>
+    Carmels: 1.50 euros<br>
+    Implementa un programa que permeti afegir càlculs a un total de compres d'aquest tipus. Per exemple, que si comprem:<br>
+    
+    2 xocolates, 1 de xiclets i 1 de carmels, tinguem un programa que permeti anar afegint els subtotals a un total, tal que així:<br>
+    
+    funció(2 xocolates) + funció(1 de xiclets) + funció(1 de carmels) = 2 + 0.5 + 1.5<br>
+    
+    Sent per tant el total, 4.<br><br>";
+
+    function totalCompra($xoco,$xiclets,$caramels){
+        $preuXoco = ($xoco * 1);
+        $preuXiclets =($xiclets * 0.5);
+        $preuCarmels = ($caramels * 1.5);
+        $total = $preuXoco + $preuXiclets + $preuCarmels;
+        echo "El preu total de la teva compra és de:<br>
+                xocolata = $preuXoco <br>
+                xiclets = $preuXiclets <br>
+                caramels = $preuCarmels <br>
+                total = $total"."€<br><br>";
+    }
+    totalCompra(2,1,1);
+    totalCompra(0,2,5);
+    totalCompra(2,2,3);
+
     ?>
 </body>
 </html>
