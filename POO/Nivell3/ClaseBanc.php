0ccacd270aeb4e2ed2f0cf0b4f0ae484dbb8c142
edverde <eduardvr83@gmail.com>
2022-03-30 12:29:47 +0200

    clase afegida al projecte

---
+++ POO/Nivell3/ClaseBanc.php
@@ -0,0 +1,55 @@
+<?php
+class Account{
+    
+    //atributs
+    var $numcompte;
+    var $nom;
+    var $cognom;
+    var $saldo;
+
+    //constructor
+    public function account($numcompte, $nom, $cognom, $saldo){
+        $this->numcompte = $numcompte;
+        $this->nom = $nom;
+        $this->cognom = $cognom;
+        $this->saldo = $saldo;
+    }
+
+    //getters
+    function getNum(){
+        return $this->numcompte;
+    }
+    function getNom(){
+        return $this->nom;
+    }
+    function getCognom(){
+        return $this->cognom;
+    }
+    function getSaldo(){
+        return $this->saldo;
+    }
+
+    //setters
+    function setNum($numcompte){
+        $this->numcompte = $numcompte;
+    }
+    function setNom($nom){
+        $this->nom = $nom;
+    }
+    function setCognom($cognom){
+        $this->cognom = $cognom;
+    }
+    function setSaldo($saldo){
+        $this->saldo = $saldo;
+    }
+    //mètodes
+    function deposit($amount){
+        $this->saldo +=$amount;
+    }
+    function withdraw($amount){
+        $this->saldo -=$amount;
+    }
+}
+$usuario = new Account(1324,'ed','va',1000);
+echo $usuario->getNom();
+?>
