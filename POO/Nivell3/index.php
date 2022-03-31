e06dbb369c2b2faea8e91d1ba2f8cfe27897ff31
edverde <eduardvr83@gmail.com>
2022-03-31 12:05:11 +0200

    mod

--- POO/Nivell3/index.php
+++ POO/Nivell3/index.php
@@ -1,14 +1,70 @@
-<?php
-include 'ClaseBanc.php';
-$usuario = new Account(1234,'eduard','valls',1000);
-echo $usuario->getSaldo();
-echo $usuario->withdraw(900);
-echo $usuario->getSaldo();
-// echo $usuario->withdraw();
-// echo $usuario->withdraw('wq');
-echo $usuario->withdraw(2);
-echo $usuario->getSaldo();
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Banco Jones</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+        <section class="formulario">
+            <form action="login.php" method="POST">
+                <h2>Banco Jones</h2>
+                <input class="controls" type="text" name="nombre" value="" placeholder="Nombre Usuario">
+                <input class="controls" type="password" name="pass" value="" placeholder="Contraseña">
+                <input class="buttons" type="submit" name="" value="Acceder">
+                <!-- <span class="cojon">&#129505;</span> -->
+                <p><a href="otc.html">¿Olvidaste la contraseña?</a></p>
+                <?php
+                if(isset($_GET['error'])){
+                    $error= $_GET['error'];
+                    if($error == "incorrecto"){
+                        echo "<h5>El usuario o contraseña son incorrectos</h5>";
+                    }
+                    if($error == 'vacio'){
+                        echo "<h5>Los datos enviados se encuentrasn vacíos.</h5>";
+                    }
+                }
+                ?>
+            </form>
+        </section>
+        
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- // $usuario = new Account(1234,'eduard','valls',1000);
+// echo $usuario->getSaldo();
+// echo $usuario->withdraw(900);
+// echo $usuario->getSaldo();
+
+// echo $usuario->withdraw(2);
+// echo $usuario->getSaldo();
 
 // https://www.youtube.com/watch?v=MX1gz_hJqz4 enlazar login con PHP
-// https://www.youtube.com/results?search_query=Validar+formulario+de+Login+con+PHP+con+POO
-?>
+// https://www.youtube.com/results?search_query=Validar+formulario+de+Login+con+PHP+con+POO -->
